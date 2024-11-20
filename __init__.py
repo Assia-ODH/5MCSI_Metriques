c86bab8e780fefdf0e0f4ec5ca33c98c6f2c7585
Assia-ODH <aouadah5@myges.fr>
2024-11-20 16:10:46 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,34 +7,21 @@ import sqlite3
                                                                                                                                        
 app = Flask(__name__)
 
+@app.route('/tawarano/')
+def meteo():
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    return jsonify(results=results)
+
 @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
-  
-@app.route('/tawarano/')
-def meteo():
-    try:
-        # Appel de l'API
-        response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
-        raw_content = response.read()
-        json_content = json.loads(raw_content.decode('utf-8'))
-        
-        # Initialisation des résultats
-        results = []
-        for list_element in json_content.get('list', []):
-            dt_value = list_element.get('dt')
-            # Sécurisation de l'accès aux données et conversion
-            temp_kelvin = list_element.get('main', {}).get('temp')
-            if temp_kelvin is not None:
-                temp_day_value = temp_kelvin - 273.15
-                results.append({'Jour': dt_value, 'temp': round(temp_day_value, 2)})  # Temp arrondie à 2 décimales
-            else:
-                results.append({'Jour': dt_value, 'temp': 'Donnée manquante'})
-        
-        return jsonify(results=results)
-
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
       
 @app.route('/')
 def hello_world():
